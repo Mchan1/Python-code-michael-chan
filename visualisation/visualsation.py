c2de307815febd8de4ce6257574c796ab121e73d
(s) Michael Chan <mchan1@pm7-0091.uopnet.plymouth.ac.uk>
2016-10-25 17:23:51 +0100

    final visual

--- visualisation/visualsation.py
+++ visualisation/visualsation.py
@@ -2,6 +2,7 @@ from graphics import *
 from time import *
 # Create a window of a suitable size
 window = GraphWin("Visualisation", 500, 500)
+
 data=[52, 47, 57, 49, 59, 62, 44, 76, 52, 52, 44, 59, 59, 79, 59, 42, 57, 48, 80, 43, 72, 74, 59, 44, 57, 55, 49, 54, 54 ]
 
 y=100
@@ -11,7 +12,8 @@ xspeed=10
 
 while True:
     for item in data:
-
+        colour=color_rgb(250,item*2,200)
+        window.setBackground(colour)
 # Create and draw a circle
 
       #  if x<499: x=x+5 
@@ -39,7 +41,9 @@ while True:
         box.setOutline(color_rgb(0,0,0))
         box.setFill(color_rgb(150,item,item))
         box.draw(window)
-        
+
+
+      
 
 
 # Wait until the mouse is clicked before closing the window
