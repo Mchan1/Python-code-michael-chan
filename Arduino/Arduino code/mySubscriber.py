2a2c5529d05678ed9cdf01c75f59a1d97ebe8563
(s) Michael Chan <mchan1@pm7-0091.uopnet.plymouth.ac.uk>
2016-10-25 16:42:28 +0100

    Lights finally working

---
+++ Arduino/Arduino code/mySubscriber.py
@@ -0,0 +1,23 @@
+from mosquitto import *
+from serial import *
+from random import *
+
+# FULL DEVICE NAME can be found by running: python PortScanner.py
+# SPEED is usually 115200 for Microbit and 9600 for Arduino
+board = Serial("/dev/cu.usbmodem14431",9600,timeout=2)
+
+randomID = random()
+client = Mosquitto("LightSubscriber" + str(randomID))
+client.connect("10.212.61.136")
+
+client.subscribe("/lights")
+
+def messageReceived(broker, obj, msg):
+    global client
+    payload = msg.payload.decode()
+    print("Message " + msg.topic + " containing: " + payload)
+    board.write(payload.encode())
+    
+client.on_message = messageReceived
+
+while True: client.loop()
